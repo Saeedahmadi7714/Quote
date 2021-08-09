bfc53bf7989985a76aa01f3da75a92f540d1d341
Sajjad Yadollahzadeh <84503973+Sajjad2117@users.noreply.github.com>
2021-08-09 10:06:06 +0430

    Update blog.py

--- quote/blog.py
+++ quote/blog.py
@@ -1,24 +1,41 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, session
 from .database import get_db
 
 bp = Blueprint("blog", __name__)
+
+db = get_db()
+
+
+class Blogpost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    subtitle = db.Column(db.String(50))
+    author = db.Column(db.String(20))
+    date_posted = db.Column(db.DateTime)
+    content = db.Column(db.Text)
 
 
 # For testing that project is correctly running
 @bp.route("/")
 def index():
-    db = get_db()
-    print(db)
-    return render_template('/blog/index.html')
+    posts = Blogpost.query.order_by(Blogpost.date_posted.desc()).all()
+    return render_template('/blog/index.html', posts=posts)
+
+
+@bp.route('/about')
+def about():
+    return render_template('about.html')
 
 
 @bp.route("/home/")
 def home():
-    pass
+    posts = Blogpost.query.filter_by().all()
+    return render_template('home.html', posts=posts)
 
 
 @bp.route('/post/<post_id>')
 def post(post_id):
+    db = get_db()
     pass
 
 
@@ -35,3 +52,9 @@ def tag(tag_id):
 @bp.route('/login')
 def login():
     pass
+
+
+@bp.route("/logout")
+def logout():
+    session.pop('user')
+    return redirect('/')
