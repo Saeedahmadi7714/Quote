6ac90c782a450ccfd9cf8f27298623c579aa0a5f
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-08-12 03:36:18 +0430

    C- Add User schema

---
+++ quote/models.py
@@ -0,0 +1,11 @@
+from mongoengine import *
+
+
+class User(Document):
+    user_name = StringField(max_length=50, required=True)
+    first_name = StringField(max_length=50, required=True)
+    last_name = StringField(max_length=50, required=True)
+    password = StringField(max_length=16, required=True)
+    email = StringField(max_length=50)
+    phone_number = StringField(max_length=15, required=True)
+    image = StringField(max_length=200, required=True)
