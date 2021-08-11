009df79fbb2ef7ec949de05ccb1828697bc07fd0
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-08-12 03:35:50 +0430

    Update and cleaning database.py

--- quote/database.py
+++ quote/database.py
@@ -1,13 +1,10 @@
-from flask import current_app
 from flask import g
-from pymongo import MongoClient
+from mongoengine import connect
 
 
 def get_db():
 
     if "db" not in g:
-        client = MongoClient('mongodb://127.0.0.1:27017')
-        db_name = current_app.config['DB_NAME']
-        g.db = client[db_name]
+        g.db = connect('blog')
 
     return g.db
