9010a55081f6ca2486c7d3a16dade5b662d72a40
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-08-16 20:49:57 +0430

    C- Complete user profile [back-end]

--- quote/user.py
+++ quote/user.py
@@ -1,12 +1,29 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from .models import User
 from .database import get_db
 
 bp = Blueprint("user", __name__, url_prefix="/user")
 
 
-@bp.route('/profile/')
+@bp.route('/profile/', methods=['GET', 'POST'])
 def profile():
-    pass
+    if request.method == 'GET':
+
+        if session is None:
+            return redirect(url_for('blog.login'))
+        else:
+            db = get_db()
+            user = User.objects(id=session['user_id']).first()
+            return render_template("user/profile.html", user=user)
+
+    db = get_db()
+    new_password = request.form.get('password')
+    user = User.objects(id=session['user_id']).first()
+    user.password = generate_password_hash(new_password, method='sha256')
+    user.save()
+    return redirect(url_for('user.profile'))
 
 
 @bp.route('/posts-list/<user_id>/')
@@ -22,3 +39,17 @@ def create_post():
 @bp.route('/edit-post/<post_id>/')
 def edit_post(post_id):
     pass
+
+
+@bp.route('/change_password/', methods=['POST'])
+def change_password():
+    if session is None:
+        return redirect(url_for('blog.login'))
+
+    db = get_db()
+    new_password = request.form.get('password')
+    user = User.objects(id=session['user_id']).first()
+    user.password = generate_password_hash(new_password, method='sha256')
+    user.save()
+    flash('Your password successfully changed.')
+    return redirect(url_for('user.profile'))
