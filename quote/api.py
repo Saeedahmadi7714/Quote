8667428f9afbc3337b139f3c0323ca09851c1ad0
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-08-19 04:33:27 +0430

    C- Complete save comment with ajax

--- quote/api.py
+++ quote/api.py
@@ -1,9 +1,30 @@
-from flask import Blueprint, render_template, session, url_for, redirect
-
-from .models import User
+from flask import Blueprint, session, url_for, redirect, request, jsonify
+from datetime import datetime
+from .models import User, Comment, Post
 from .database import get_db
 
 bp = Blueprint("api", __name__)
+
+
+@bp.route('/create_comment/', methods=['POST'])
+def create_comment():
+    if request.method == 'POST':
+        db = get_db()
+        comment = request.form.get('comment')
+        post_id = request.form.get('postId')
+        user_id = request.form.get('userId')
+
+        post = Post.objects(id=post_id).first()
+
+        # Create a comment
+        user = User.objects(id=user_id).first()
+        new_comment = Comment(owner=user.id, text=comment, created_date=datetime.now())
+        new_comment.save()
+
+        post.comments.append(new_comment)
+        post.save()
+        print(post.comments)
+        return jsonify(comment)
 
 
 @bp.route('/posts_list/<user_id>/')
@@ -41,6 +62,7 @@ def user_profile(user_id):
     pass
 
 
+@bp.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for("blog.home"))
