a8a680f4a5c572049f5e3ae1aa508577fea41b75
Saeedahmadi <saeedahmadi7714@gmail.com>
2021-08-21 18:33:19 +0430

    C- Change ajax url prefix

--- quote/static/js/edit_post.js
+++ quote/static/js/edit_post.js
@@ -2,16 +2,18 @@ $(document).ready(function () {
     $('#delete-btn').on('click', function () {
 
         const postId = $('#postId').html()
-
+        console.log(postId);
         $.ajax({
             type: 'POST',
-            url: '/post-delete/' + postId + '/',
+            url: '/api/post-delete/' + postId + '/',
             processData: false,
             contentType: false,
             cache: false,
             success: function () {
                 const a = $(`#${postId}`).remove()
+
                 alert("Your post successfully deleted.");
+
 
             },
             error: function (e) {
