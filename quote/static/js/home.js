309cebbb5bee2016aaf2b6d9bc86b0cd28759eb9
erfanghorbanee <erfanghorbanee@gmail.com>
2021-08-26 08:38:33 +0430

    A- bugs fixed.

--- quote/static/js/home.js
+++ quote/static/js/home.js
@@ -19,14 +19,16 @@ function post_by_tag(id) {
 
             // add posts by tag
             for (let post in data) {
+                var title = data[post].title
+                var title2 = title.charAt(0).toUpperCase() + title.slice(1);
                 // console.log(data[post]._id);
 
                 var content = `<div class="col-md-12 ">
                 <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative ">
                     <div class="col p-4 d-flex flex-column position-static ">
-                        <h3 class="mb-0 ">` + data[post].title + `</h3>
+                        <h3 class="mb-0 ">` + title2 + `</h3>
                         <div class="mb-1 text-muted ">` + data[post].pub_date + `</div>
-                        <p class="card-text mb-auto ">` + data[post].content + `</p>
+                        <p class="card-text mb-auto ">` + data[post].content.substring(0, 300) + `</p>
                         <a href="/post/` + data[post]._id + `" class="stretched-link ">Continue
                                     reading</a>
                     </div>
